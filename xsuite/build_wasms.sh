3c98d2caeb5e1770d02961c12adf1881afb2f9f4
lcswillems <lcswillems@gmail.com>
2023-07-06 11:49:09 +0200

    Add verification of xsuite wasms in CI

---
+++ xsuite/build_wasms.sh
@@ -0,0 +1,22 @@
+SOURCE_DIR="$(pwd)/contracts"
+DEST_DIR="/tmp/xsuite-contracts"
+
+rm -rf $DEST_DIR
+cp -R $SOURCE_DIR $DEST_DIR
+
+cd $DEST_DIR
+
+sc-meta all build --locked --target-dir $(pwd)/target
+
+shopt -s globstar dotglob
+
+for file in ./**/*.wasm
+do
+  if [ -f "$file" ]; then
+    destination=$SOURCE_DIR/${file#./}
+    mkdir -p "$(dirname "$destination")"
+    cp "$file" "$destination"
+  fi
+done
+
+shopt -u globstar dotglob
