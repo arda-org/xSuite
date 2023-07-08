0d64195cc16b3c80319c79ce140a2b8b62accd65
lcswillems <lcswillems@gmail.com>
2023-07-08 17:15:02 +0200

    Improve initial test of blank

--- contracts/blank/tests/contract.test.ts
+++ contracts/blank/tests/contract.test.ts
@@ -2,25 +2,25 @@ import { test, beforeEach, afterEach } from "node:test";
 import { assertAccount } from "xsuite/test";
 import { FWorld, FWorldWallet, FWorldContract } from "xsuite/world";
 
-let fworld: FWorld;
+let world: FWorld;
 let deployer: FWorldWallet;
 let contract: FWorldContract;
 
 beforeEach(async () => {
-  fworld = await FWorld.start();
-});
-
-afterEach(() => {
-  fworld.terminate();
-});
-
-test("Test", async () => {
-  deployer = await fworld.createWallet({ balance: 10_000_000_000n });
+  world = await FWorld.start();
+  deployer = await world.createWallet();
   ({ contract } = await deployer.deployContract({
     code: "file:output/contract.wasm",
     codeMetadata: [],
     gasLimit: 10_000_000,
   }));
+});
+
+afterEach(async () => {
+  await world.terminate();
+});
+
+test("Test", async () => {
   assertAccount(await contract.getAccountWithPairs(), {
     balance: 0n,
     hasPairs: [],
