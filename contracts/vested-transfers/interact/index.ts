fc0d2ab491404e9cfc93768204356f549cf3e5d2
lcswillems <lcswillems@gmail.com>
2023-07-13 15:43:10 +0200

    Rewrite interactions of vested-transfers

---
+++ contracts/vested-transfers/interact/index.ts
@@ -0,0 +1,47 @@
+import { Command } from "commander";
+import { envChain } from "xsuite/interact";
+import { World } from "xsuite/world";
+import data from "./data.json";
+
+const world = World.new({
+  proxyUrl: envChain.publicProxyUrl(),
+  chainId: envChain.id(),
+  gasPrice: 1000000000,
+});
+
+const loadWallet = () => world.newWalletFromFile("wallet.json");
+
+const program = new Command();
+
+program.command("deploy").action(async () => {
+  const wallet = await loadWallet();
+  const result = await wallet.deployContract({
+    code: data.code,
+    codeMetadata: ["upgradeable"],
+    gasLimit: 20_000_000,
+  });
+  console.log("Result:", result);
+});
+
+program.command("upgrade").action(async () => {
+  const wallet = await loadWallet();
+  const result = await wallet.upgradeContract({
+    callee: envChain.select(data.address),
+    code: data.code,
+    codeMetadata: ["upgradeable"],
+    gasLimit: 20_000_000,
+  });
+  console.log("Result:", result);
+});
+
+program.command("ClaimDeveloperRewards").action(async () => {
+  const wallet = await loadWallet();
+  const result = await wallet.callContract({
+    callee: envChain.select(data.address),
+    funcName: "ClaimDeveloperRewards",
+    gasLimit: 10_000_000,
+  });
+  console.log("Result:", result);
+});
+
+program.parse(process.argv);
