bc41d9f54da30103164d33dc46fccafd6c6c4dc2
Rares <6453351+raress96@users.noreply.github.com>
2024-04-26 16:13:58 +0300

    Lint fixes.

--- xsuite-chainsimulator/index.js
+++ xsuite-chainsimulator/index.js
@@ -2,8 +2,8 @@ const path = require("node:path");
 
 function getCsproxyBinPath() {
   switch (process.platform) {
-    case 'linux':
-      return path.join(__dirname, 'bin', 'csproxy-linux-amd64');
+    case "linux":
+      return path.join(__dirname, "bin", "csproxy-linux-amd64");
     case "darwin":
       return path.join(__dirname, "bin", "csproxy-darwin-amd64");
     default:
@@ -12,7 +12,7 @@ function getCsproxyBinPath() {
 }
 
 function getCsproxyDefaultConfigPath() {
-  return path.join(__dirname, 'config');
+  return path.join(__dirname, "config");
 }
 
 module.exports = { getCsproxyBinPath, getCsproxyDefaultConfigPath };
