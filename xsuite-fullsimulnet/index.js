9c427076188e86f8c98bcd80eb378cf7ccd745d2
lcswillems <lcswillems@gmail.com>
2024-05-30 17:10:55 +0200

    Make dependencies optional

--- xsuite-fullsimulnet/index.js
+++ xsuite-fullsimulnet/index.js
@@ -1,13 +1,12 @@
 const path = require("node:path");
 
 function getFsproxyBinPath() {
-  switch (process.platform) {
-    case "linux":
-      return require.resolve("@xsuite/full-simulnet-linux-amd64/bin/fsproxy");
-    case "darwin":
-      return require.resolve("@xsuite/full-simulnet-darwin-amd64/bin/fsproxy");
-    default:
-      throw new Error(`Unsupported platform: ${process.platform}`);
+  if (process.platform === "linux" && process.arch === "x64") {
+    return require.resolve("@xsuite/full-simulnet-linux-amd64/bin/fsproxy");
+  } else if (process.platform === "darwin" && process.arch === "x64") {
+    return require.resolve("@xsuite/full-simulnet-darwin-amd64/bin/fsproxy");
+  } else {
+    throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
   }
 }
 
--- xsuite-lightsimulnet/index.js
+++ xsuite-lightsimulnet/index.js
@@ -1,11 +1,10 @@
 function getLsproxyBinPath() {
-  switch (process.platform) {
-    case "linux":
-      return require.resolve("@xsuite/light-simulnet-linux-amd64/bin/lsproxy");
-    case "darwin":
-      return require.resolve("@xsuite/light-simulnet-darwin-amd64/bin/lsproxy");
-    default:
-      throw new Error(`Unsupported platform: ${process.platform}`);
+  if (process.platform === "linux" && process.arch === "x64") {
+    return require.resolve("@xsuite/light-simulnet-linux-amd64/bin/lsproxy");
+  } else if (process.platform === "darwin" && process.arch === "x64") {
+    return require.resolve("@xsuite/light-simulnet-darwin-amd64/bin/lsproxy");
+  } else {
+    throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
   }
 }
 
