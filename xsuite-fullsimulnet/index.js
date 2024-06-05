ec8ee4ed154b6b7ebe492ec49736ff01b2febbfd
lcswillems <lcswillems@gmail.com>
2024-06-05 19:12:39 +0200

    Authorize arm64

--- xsuite-fullsimulnet/index.js
+++ xsuite-fullsimulnet/index.js
@@ -1,11 +1,12 @@
 function getFsproxyBinPath() {
-  if (process.platform === "linux" && process.arch === "x64") {
-    return require.resolve("@xsuite/full-simulnet-linux-amd64/bin/fsproxy");
-  } else if (process.platform === "darwin" && process.arch === "x64") {
-    return require.resolve("@xsuite/full-simulnet-darwin-amd64/bin/fsproxy");
-  } else {
-    throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
+  if (process.arch === "x64" || process.arch === "arm64") {
+    if (process.platform === "linux") {
+      return require.resolve("@xsuite/full-simulnet-linux-amd64/bin/fsproxy");
+    } else if (process.platform === "darwin") {
+      return require.resolve("@xsuite/full-simulnet-darwin-amd64/bin/fsproxy");
+    }
   }
+  throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
 }
 
 module.exports = { getFsproxyBinPath };
--- xsuite-lightsimulnet/index.js
+++ xsuite-lightsimulnet/index.js
@@ -1,11 +1,12 @@
 function getLsproxyBinPath() {
-  if (process.platform === "linux" && process.arch === "x64") {
-    return require.resolve("@xsuite/light-simulnet-linux-amd64/bin/lsproxy");
-  } else if (process.platform === "darwin" && process.arch === "x64") {
-    return require.resolve("@xsuite/light-simulnet-darwin-amd64/bin/lsproxy");
-  } else {
-    throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
+  if (process.arch === "x64" || process.arch === "arm64") {
+    if (process.platform === "linux") {
+      return require.resolve("@xsuite/light-simulnet-linux-amd64/bin/lsproxy");
+    } else if (process.platform === "darwin") {
+      return require.resolve("@xsuite/light-simulnet-darwin-amd64/bin/lsproxy");
+    }
   }
+  throw new Error(`Unsupported config: ${process.platform} ${process.arch}`);
 }
 
 module.exports = { getLsproxyBinPath };
