2749900af505edaf4612bcef66b6bf342801e7b1
Jad Zahreddine <107253062+jad-zd@users.noreply.github.com>
2024-06-29 16:58:57 +0200

    Temporary patch to the xsuite test 'new --dir contract && build --locked && build -r && test-rust && test-scen' where we now install the version of rust used on main, run the tests, then switch back to the default version 1.79.0

--- xsuite/src/cli/cli.test.ts
+++ xsuite/src/cli/cli.test.ts
@@ -347,12 +347,23 @@ test.concurrent(
   async () => {
     using c = newContext();
 
+    // Switch to a rust version compatible with sample contracts on main branch.
+    await c.cmd('install-rust --toolchain "nightly-2023-12-11"');
+
     const dir = "contract";
     await c.cmd(`new --dir ${dir}`);
     expect(fs.readdirSync(c.cwd()).length).toEqual(1);
     const starterChalk = chalk.magenta("blank");
     const absDirPath = path.resolve(c.cwd(), dir);
     expect(c.flushStdout().split("\n")).toEqual([
+      //// Must be removed when removing rust version switch ////
+      chalk.blue(
+        'Installing Rust: toolchain "nightly-2023-12-11" & target wasm32-unknown-unknown...',
+      ),
+      chalk.cyan(
+        '$ curl --proto =https --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain "nightly-2023-12-11" -t wasm32-unknown-unknown -y',
+      ),
+      ///////////////////////////////////////////////////////////
       chalk.blue(`Downloading contract ${starterChalk} in "${absDirPath}"...`),
       "",
       chalk.blue("Installing packages..."),
@@ -426,6 +437,9 @@ test.concurrent(
       chalk.cyan(`$ ${binaryPath} .`),
       "",
     ]);
+
+    // Switch back to the default rust version.
+    await c.cmd("install-rust");
   },
   100_000,
 );
