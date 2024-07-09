6238546f16d341fe5479562984d3f7304e1db6ea
lcswillems <lcswillems@gmail.com>
2024-07-09 17:55:29 +0200

    Upgrade chain simulator to v1.7.13

--- xsuite-fullsimulnet/build-binary.mjs
+++ xsuite-fullsimulnet/build-binary.mjs
@@ -10,5 +10,5 @@ if (process.env.CI) {
   ]);
 }
 
-await $`GOBIN="$(pwd)" go install -ldflags ${argv.ldflags} github.com/multiversx/mx-chain-simulator-go/cmd/chainsimulator@v1.7.12-rc1`;
+await $`GOBIN="$(pwd)" go install -ldflags ${argv.ldflags} github.com/multiversx/mx-chain-simulator-go/cmd/chainsimulator@v1.7.13`;
 await $`mv ./chainsimulator ../xsuite-fullsimulnet-${argv.os}-${argv.arch}/bin/fsproxy`;
